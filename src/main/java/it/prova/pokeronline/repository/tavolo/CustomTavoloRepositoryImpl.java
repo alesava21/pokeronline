53908710a9ce7af7693b72f57e94cd08d4862b3c
System Administrator <root@MacBook-Air-di-Alessandro.local>
2022-11-29 21:22:59 +0100

    implementazione findByExample di tavolo anche con l'utente special

--- src/main/java/it/prova/pokeronline/repository/tavolo/CustomTavoloRepositoryImpl.java
+++ src/main/java/it/prova/pokeronline/repository/tavolo/CustomTavoloRepositoryImpl.java
@@ -13,21 +13,20 @@ import org.apache.commons.lang3.StringUtils;
 
 import it.prova.pokeronline.model.Tavolo;
 
-public class CustomTavoloRepositoryImpl implements CustomTavoloRepository{
-	
+public class CustomTavoloRepositoryImpl implements CustomTavoloRepository {
+
 	@PersistenceContext
 	private EntityManager entityManager;
 
-	
 	/*
 	 * FindByExample per l'admin, Si puo anche ricercare utentechecreailtavolo_id
 	 * Passando il giusto id.
-	 * */
+	 */
 	@Override
 	public List<Tavolo> findByExample(Tavolo example) {
 		Map<String, Object> paramaterMap = new HashMap<String, Object>();
 		List<String> whereClauses = new ArrayList<String>();
-		
+
 		StringBuilder queryBuilder = new StringBuilder("select t from Tavolo t where t.id = t.id ");
 
 		if (StringUtils.isNotEmpty(example.denominazione())) {
@@ -51,7 +50,7 @@ public class CustomTavoloRepositoryImpl implements CustomTavoloRepository{
 			whereClauses.add(" t.utentechecreailtavolo_id.id = :utentechecreailtavolo_id ");
 			paramaterMap.put("utentechecreailtavolo_id", example.utenteCheCreaIlTavolo().id());
 		}
-		
+
 		queryBuilder.append(!whereClauses.isEmpty() ? " and " : "");
 		queryBuilder.append(StringUtils.join(whereClauses, " and "));
 		TypedQuery<Tavolo> typedQuery = entityManager.createQuery(queryBuilder.toString(), Tavolo.class);
@@ -63,10 +62,51 @@ public class CustomTavoloRepositoryImpl implements CustomTavoloRepository{
 		return typedQuery.getResultList();
 	}
 
+	/*
+	 * FindByExample per il SpecialUser, trova anche i tavoli corrispondenti all
+	 * example in base all'utente loggato in sessione
+	 * 
+	 * 
+	 *
+	 *
+	 */
+
 	@Override
 	public List<Tavolo> findByExampleSpecialPlayer(Tavolo example, Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, Object> paramaterMap = new HashMap<String, Object>();
+		List<String> whereClauses = new ArrayList<String>();
+
+		StringBuilder queryBuilder = new StringBuilder("select t from Tavolo t where t.id = t.id ");
+
+		if (StringUtils.isNotEmpty(example.denominazione())) {
+			whereClauses.add(" t.denominazione  like :denominazione ");
+			paramaterMap.put("denominazione", "%" + example.denominazione() + "%");
+		}
+		if (example.cifraMinima() != null && example.cifraMinima() > 0) {
+			whereClauses.add(" t.cifraMinima > :cifraMinima ");
+			paramaterMap.put("cifraMinima", example.cifraMinima());
+		}
+		if (example.esperienzaMinima() != null && example.esperienzaMinima() > 0) {
+			whereClauses.add(" t.esperienzaMinima > :esperienzaMinima ");
+			paramaterMap.put("esperienzaMinima", example.esperienzaMinima());
+		}
+		if (example.dataCreazione() != null) {
+			whereClauses.add("t.dataCreazione>= :dataCreazione ");
+			paramaterMap.put("dataCreazione", example.dataCreazione());
+		}
+
+		whereClauses.add(" t.utentechecreailtavolo_id.id = :utentechecreailtavolo_id ");
+		paramaterMap.put("utentechecreailtavolo_id", id);
+
+		queryBuilder.append(!whereClauses.isEmpty() ? " and " : "");
+		queryBuilder.append(StringUtils.join(whereClauses, " and "));
+		TypedQuery<Tavolo> typedQuery = entityManager.createQuery(queryBuilder.toString(), Tavolo.class);
+
+		for (String key : paramaterMap.keySet()) {
+			typedQuery.setParameter(key, paramaterMap.get(key));
+		}
+
+		return typedQuery.getResultList();
 	}
 
 }
